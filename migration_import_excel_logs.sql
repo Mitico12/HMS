22d2580c524a32f382f50320e80455d3f7d5b49b
Mitico12 <133810769+Mitico12@users.noreply.github.com>
2026-06-29 02:36:30 +0200

    Delete migration_import_excel_logs.sql

--- migration_import_excel_logs.sql
+++
@@ -1,7 +0,0 @@
--- Allows admins to import archived report logs from the Excel format exported by Logs.
--- Run after schema.sql and migration_report_types_archive.sql.
-
-drop policy if exists inc_admin_insert on public.incidents;
-create policy inc_admin_insert on public.incidents
-  for insert
-  with check (public.is_admin());
