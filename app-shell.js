ca5fcc32b4130dc7d2b1d4a74c8dca814d89bd5b
Mitico12 <133810769+Mitico12@users.noreply.github.com>
2026-06-30 19:31:57 +0200

    Add files via upload

--- app-shell.js
+++ app-shell.js
@@ -1,4 +1,4 @@
-const SHELL_VERSION = 'hms-shell-v8';
+const SHELL_VERSION = 'hms-shell-v11';
 const SHELL_ASSETS = [
   './index.html',
   './user.html',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hms-shell-v8';
+const CACHE_NAME = 'hms-shell-v11';
 const SHELL_ASSETS = [
   './',
   './index.html',
